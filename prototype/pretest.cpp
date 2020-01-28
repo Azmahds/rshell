0371fccabab8477f92227412c1260e754bf4a618
Smit417263 <smitp8720@gmail.com>
2020-01-28 15:52:10 -0800

    upadated pretest

--- prototype/pretest.cpp
+++ prototype/pretest.cpp
@@ -1,1 +1,13 @@
-//FOR TESTING WAITPID(), FORK(), AND EXECVP()
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+        pid_t n = fork();
+        if (n != 0) {cout << "Hello Parent" <<  endl;}
+        else {waitpid(n, NULL, WNOHANG); cout << "Hello Child" << endl;}
+return 0;
+}
