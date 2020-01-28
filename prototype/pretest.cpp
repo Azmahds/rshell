cfce5320eed124ca34d51627f9ec6e2d88a2da94
Azmahds <56281148+Azmahds@users.noreply.github.com>
2020-01-27 16:56:28 -0800

    Added some tests

--- prototype/pretest.cpp
+++ prototype/pretest.cpp
@@ -1,1 +1,30 @@
 //FOR TESTING WAITPID(), FORK(), AND EXECVP()
+
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+        pid_t n = fork();
+        if (n == 0) {cout << "Hello Child" <<  endl;}
+        else if(n > 0) {waitpid(n, NULL, WNOHANG); cout << "Hello Parent" << endl;}
+        else {cout << "Error" << endl;}
+
+        //int execvp(argv[2], argv[]);
+        //cout << "END" << endl;
+
+//      pid_t child_pid;
+//      child_pid = fork ();
+//      if (child_pid != 0){
+//              cout << "parent" <<endl;
+//      }
+//      else{
+//              waitpid(child_pid, NULL,WNOHANG);
+//              cout << "after wait child"<<endl;
+//      }
+
+  return 0;
+}
