f20e86c42089635315e979e01439ea260961aa50
Smit417263 <56094552+Smit417263@users.noreply.github.com>
2020-01-28 17:46:57 -0800

    adding parse.cpp

---
+++ prototype/parse.cpp
@@ -0,0 +1,36 @@
+#include  <stdio.h>
+#include  <sys/types.h>
+#include <cstring>
+#include <iostream>
+#include <unistd.h>
+
+
+using namespace std;
+
+void  parse(char *cmd, char **toks){
+     while (*cmd != '\0') {   
+          while (*cmd == ' '){
+               *cmd++ = '\0';
+          }
+
+          *toks++ = cmd;
+
+          while (*cmd != '\0' && *cmd != ' '){
+               cmd++;
+          }
+     }
+}
+
+int main(){
+     char  cmd[500];             
+     char  *toks[50];              
+
+     while ((strcmp(toks[0], "exit") != 0)) {                   
+          cout << "$" ;  
+          gets(cmd); 
+          cout << endl;
+          parse(cmd, toks); 
+     }
+
+return 0;
+}
