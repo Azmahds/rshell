180bb1c546a8ddda75e04d9874928568e7abbf54
Azmahds <azmahds@gmail.com>
2020-02-08 16:08:39 -0800

    edits to Token.h

--- src/Token.h
+++ src/Token.h
@@ -11,7 +11,13 @@ class Token : public CMD{
 	public:
 		virtual bool run();
 		Token();
-
+		Token(char* arr[], int size) {
+			for(int i = 0; i < size; ++i){
+				toks[i] = arr[i];
+			}
+		};
+	protected:
+		char* toks[64];
 };
 
 #endif
