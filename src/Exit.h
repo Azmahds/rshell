73a51f857a62b3f996ec5c0fb958f85effb526ed
Azmahds <azmahds@gmail.com>
2020-02-08 16:20:00 -0800

    added a couple subclasses

---
+++ src/Exit.h
@@ -0,0 +1,19 @@
+#ifndef EXIT_H
+#define EXIT_H
+
+#include "Token.h"
+
+#include <iostream>
+
+usign namespace std;
+
+class Exit : public Token{
+        public:
+                bool run();
+                Exit() {ex[0] = '\n';};
+		Exit(char* arr[]) {ex[0] = '\n';};
+	Protected:
+	char* ex[6];
+};
+
+#endif
