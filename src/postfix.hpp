d6c043890187df123b2307a534caeace92467a7b
Azmahds <azmahds@gmail.com>
2020-02-13 12:28:25 -0800

    edit to postfix

--- src/postfix.hpp
+++ src/postfix.hpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+/*
 void buildTree(){
   string s;
   s = infix_to_postfix();
@@ -30,7 +31,7 @@ void buildTree(){
   root = r.top();
   r.pop();
 }
-
+*/
 
 string infix_to_postfix(char** input){
    stack<char*> s;
@@ -42,36 +43,53 @@ string infix_to_postfix(char** input){
    }
    
    int index = 0;
-   int s1 = -1;
-   int s2 = -1;
+   int i1 = 0;
    char *arr[SIZE];
    char *aSym = (char*) "&&";
    char *oSym = (char*) "||";
    char *sSym = (char*) ";"; 
 
-   for(int i = 1; i <= SIZE; ++i){
-	//bool one = false, two = false;
-        //int s1 = -1;
-   	//int s2 = -1;
-	char* t2 = input[i];
-	char* t1 = input[i-1];
-	//for(unsigned j = 0; t1[j] != '\0'; ++j){
-	//	if(t1[j] == ';'){
-	//		s1 = j;
-	//		one = true;
-	//	}
-	//}
+   for(int i = 0; i < SIZE; ++i){
 	if(strcmp(input[i], aSym) == 0){
- 					
+ 	    char *sum = new char[512];
+	    for(int l = i1; l < i; ++l){
+		strcat(sum, input[l]);
+		if(l+1 !=  i){strcat(sum, ' ');}
+	    }
+            i1 = i + 1;
+	    arr[index] = sum;
+	    ++index;
+	    arr[index] = new (char*) "&&";
+	    ++index;
         }
         else if(strcmp(input[i], oSym) == 0){
-
+	    char *sum  = new  char[512];
+            for(int l = i1; l < i; ++l){
+                strcat(sum, input[l]);
+                if(l+1 !=  i){strcat(sum, ' ');}
+            }
+            i1 = i + 1;
+            arr[index] = sum;
+            ++index;
+	    arr[index] = new (char*) "||";
+            ++index;
         }
         else if(strcmp(input[i], sSym) == 0){
-
+		cout << "L SNZ BOIZZZZ" << endl;
         }	
    }
-    
+   if(i1 < SIZE){
+ 	char *sum = new char[512];
+        while (i1 < SIZE){
+	     strcat(sum, input[i1];
+	     ++i1;
+	}
+	arr[index] = sum;
+	++index;
+   {
+   arr[index] = '\0';
+   SIZE = index;   
+ 
    for(unsigned i = 0; i < input.size();++i){
        c = input.at(i);
        if(c == ' '){
@@ -99,6 +117,8 @@ string infix_to_postfix(char** input){
    return oss.str();
  }
 
+
+/*
 void infix(TreeNode *node){ //left cur right
   if(node == NULL){
     return;
@@ -116,3 +136,4 @@ void infix(TreeNode *node){ //left cur right
     cout << ")";
   }
 }
+*/
