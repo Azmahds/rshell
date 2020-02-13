cd47cb7c9e2e2aa93ca645287d4626ccfc514941
Azmahds <azmahds@gmail.com>
2020-02-12 20:55:26 -0800

    single command working

--- src/test.cpp
+++ src/test.cpp
@@ -15,61 +15,43 @@
 
 using namespace std;
 
-vector<char*> parse(char* token);
-bool prototype(vector<char*> toks);
-vector<CMD*> SetClasses(vector<char*> cline);
+char** parse(string);
+bool prototype(char** toks);
 
 int main(){
   char *cmd;
-  vector<char*> tokens;
   string input;
   bool run;
   vector<CMD*> command;
+  vector<char*> t;
+  
+  while(input != "exit"){
+ 	 cout << "$ ";
+ 	 getline(cin,input);
 
-  cout << "$ ";
-  getline(cin,input);
-
-  if(input == "exit"){
-      cout << "exiting now" << endl;
+ 	 if(input == "exit"){
+ 	     break;
+ 	 }
+  	 char **arr;
+  	 arr = parse(input);
+ 	if(prototype(arr)){cout << "SUCCESS" << endl;}
+	else{cout << "FAIL" << endl;}
   }
 
-  char arr[input.size()];
-  for(int i = 0; i < input.size(); ++i){
-    arr[i] = input.at(i);
-  }
-  arr[input.size()] = '\0';
 
-  cmd = arr;
-
-  tokens = parse(cmd);
-  
-  command = SetClasses(tokens);
-  
   return 0;
-}
+}	
 
-vector<CMD*> SetClasses(vector<char*> cline){
-  vector<CMD*> cmd;
-  char *arr[cline.size()];
- 
-  for(unsigned i = 0; i < cline.size(); ++i){
-	arr[i] = cline.at(i);
-  }
-  arr[cline.size()] = '\0';
+char** parse(string input){
+   char *token = new char [input.size()];
+   for(int i = 0; i < input.size(); ++i){
+    	token[i] = input.at(i);
+   }
+   token[input.size()] = '\0';    
 
- // CMD* temp = new Token();
-//  temp->SetCmd(arr);
-//  cmd.push_back(temp);
-  
-
-  return cmd;  
-}
-	
-
-vector<char*> parse(char *token){
-    vector<char*> t;
-    int SIZE = 0;
-
+   vector<char*> t;
+  //  vector<CMD*> c;
+    int SIZE = 0;	
     while(token[SIZE] != '\0'){
       ++SIZE;
     }
@@ -81,21 +63,26 @@ vector<char*> parse(char *token){
     }
 
     token = strtok(token, "+");
-
+    
     while(token != NULL){
       t.push_back(token);
       token = strtok(NULL, "+");
     }
-    return t;
+    
+    char **array = new char*[t.size()];
+    for(unsigned i = 0; i < t.size(); ++i){
+         array[i] = t.at(i);
+    }
+    array[t.size()] = '\0';
+    return array;
 }
 
 
 
-bool prototype(vector<char*> toks){
+bool prototype(char** toks){
   pid_t pid;
   pid_t wpid;
   int status;
-  char *cmd[toks.size()];
 
   pid = fork();
   if (pid < 0){
@@ -105,13 +92,18 @@ bool prototype(vector<char*> toks){
 
   switch(pid){
           case 0:
-        			execvp(cmd[0], cmd);
-        			perror("EXECVP FAILURE");
-        			exit(false);
+        	if(execvp(toks[0], toks) != 0){
+        		perror("EXECVP FAILURE");
+        		exit(false);
+		}
+		exit(true);
           default:
-                  while(!WIFEXITED(status) && !WIFSIGNALED(status)){
-                          wpid = waitpid(pid, &status, WUNTRACED);
+                  if (waitpid(pid, &status, WUNTRACED) < 0){
+                          perror("CHILD IN PROGRESS");
                   }
+		  if(WIFEXITED(status)){
+			return true;
+		  }
   }
 	return true;
 }
