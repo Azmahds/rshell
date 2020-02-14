2c6a3c07ec70663e4ecceb900912c7832f4532ef
Azmahds <azmahds@gmail.com>
2020-02-13 20:58:56 -0800

    added tests

--- unit_tests/unit_test.cpp
+++ unit_tests/unit_test.cpp
@@ -14,7 +14,12 @@
 #include "../src/Semicolon.cpp"
 //#include "src/postfix.hpp"
 
-
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <stdio.h>
+#include <unistd.h>
+using namespace std;
 
 
 TEST(AndTest, AndDisplay){
@@ -54,15 +59,41 @@ TEST(TokenTest, TokenRunEcho){
     arr[1] = 'c';
     arr[2] = 'h';
     arr[3] = 'o';
-    arr[4] = '\0';
+    arr[4] = ' ';
+    arr[5] = '"';
+    arr[6] = 'h';
+    arr[7] = 'e';
+    arr[8] = 'l';
+    arr[9] = 'l';
+    arr[10] = 'o';
+    arr[11] = '"';
+    arr[12] = '\0';
     Token* f = new Token(arr );
     EXPECT_EQ(f->run(), true);
 }
 
-
-
-
-
+TEST(OrTest, OrRun){
+    char* arr1 = new char[32];
+    arr1[0] = 'l';
+    arr1[1] = 's';
+    arr1[2] = '\0';
+    
+    char* arr = new char[32];
+    arr[0] = 'e';
+    arr[1] = 'c';
+    arr[2] = 'h';
+    arr[3] = 'o';
+    arr[4] = ' ';
+    arr[5] = 'h';
+    arr[6] = 'i';
+    arr[7] = '\0';
+    Or* o = new Or();
+    Token* f = new Token(arr);
+    o->SetL(f);
+    f = new  Token(arr1);
+    o->SetR(f);   
+    EXPECT_EQ(o->run(), true);
+}
 
 
 int main(int argc, char **argv) {
