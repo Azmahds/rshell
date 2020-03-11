458071da885b4772c3a25d8720177914dc07f165
Azmahds <azmahds@gmail.com>
2020-03-11 14:46:51 -0700

    edits to run

--- src/OutRed.cpp
+++ src/OutRed.cpp
@@ -73,12 +73,13 @@ bool OutRed::run() {
 	}
 	
 	else if((L->isCon()) &&  !(R->isCon())){
-		string lhs = L->GetR()->GetFullTok();
 		string hs = R->GetFullTok();
                 string rhs;
                 string w = "w";
                 string r  = "r";
                 string a = "a";
+	
+		char *lhs = L->execute();
 
                 char* rArr = (char*)  ">";
                 char* drArr = (char*) ">>";
@@ -87,13 +88,6 @@ bool OutRed::run() {
                 if(strcmp(rArr, temp) == 0){rhs =   "> " + hs;}
                 else if(strcmp(drArr, temp) == 0){rhs =  ">> " + hs;}
 
-                const int max = 256;
-
-                char BUF[max];
-
-                memset(BUF, '\0', max);
-
-                FILE* com1 = popen(lhs.c_str(), r.c_str());
                 FILE* com2;
 
                 if(rhs.at(0) == '>' && rhs.at(1) == '>'){
@@ -103,19 +97,17 @@ bool OutRed::run() {
                          com2 = fopen(hs.c_str(), w.c_str());
                 }
 
-                if(com1 == nullptr || com2 == nullptr){return false;}
+                if(com2 == nullptr){ delete [] lhs; return false;}
+                
+                fputs(lhs, com2);
 
-                while(fgets(BUF, max, com1) != nullptr){
-                        fputs(BUF, com2);
-                }
-
-                pclose(com1);
                 fclose(com2);
 
+		delete [] lhs;
                 return true;
 	}
 
-	else if(!(L->isCon()) &&  (R->isCon())){
+	else if(!(L->isCon()) && (R->isCon())){
                 string lhs = L->GetFullTok();
                 string hs = R->GetL()->GetFullTok();
                 string rhs;
