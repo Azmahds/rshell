9b9d73917dac9108259c2385a15034f1ec3f7dd6
Smit417263 <smitp8720@gmail.com>
2020-03-11 18:29:53 -0700

    edited run func

--- src/InRed.cpp
+++ src/InRed.cpp
@@ -5,31 +5,31 @@ bool InRed::run() {
 	CMD* L = this->GetL();
 	CMD* R = this->GetR();
 
-	if(!(L->isCon()) && !(R->isCon())){
-		string lhs = L->GetFullTok();
-		string rhs = R->GetFullTok();	
 
-		string fin = lhs + " < " + rhs;
+	string lhs = L->GetFullTok();
+	string rhs = R->GetFullTok();	
 
-		string r = "r";
+	string fin = lhs + " < " + rhs;
+
+	string r = "r";
 	
-		const int max = 256;
+	const int max = 256;
 	
-		char BUF[max];
+	char BUF[max];
 
-		memset(BUF, '\0', max);
+	memset(BUF, '\0', max);
 		
-		FILE* com = popen(fin.c_str(), r.c_str());
+	FILE* com = popen(fin.c_str(), r.c_str());
 
-		if(com == nullptr){return false;}
+	if(com == nullptr){return false;}
 		
-		while(fgets(BUF, max, com) != nullptr){
-			cout << BUF;
-		}
+	while(fgets(BUF, max, com) != nullptr){
+		cout << BUF;
+	}
 		
-		pclose(com);
+	pclose(com);
 
-		return true;
+	return true;
 	}
 }
 
--- src/OutRed.cpp
+++ src/OutRed.cpp
@@ -106,48 +106,5 @@ bool OutRed::run() {
 		delete [] lhs;
                 return true;
 	}
-
-	else if(!(L->isCon()) && (R->isCon())){
-                string lhs = L->GetFullTok();
-                string hs = R->GetL()->GetFullTok();
-                string rhs;
-                string w = "w";
-                string r  = "r";
-                string a = "a";
-
-                char* rArr = (char*)  ">";
-                char* drArr = (char*) ">>";
-                char* temp = cons[0];
-
-                if(strcmp(rArr, temp) == 0){rhs =   "> " + hs;}
-                else if(strcmp(drArr, temp) == 0){rhs =  ">> " + hs;}
-
-                const int max = 256;
-
-                char BUF[max];
-
-                memset(BUF, '\0', max);
-
-                FILE* com1 = popen(lhs.c_str(), r.c_str());
-                FILE* com2;
-
-                if(rhs.at(0) == '>' && rhs.at(1) == '>'){
-                         com2 = fopen(hs.c_str(), a.c_str());
-                }
-                else{
-                         com2 = fopen(hs.c_str(), w.c_str());
-                }
-
-                if(com1 == nullptr || com2 == nullptr){return false;}
-
-                while(fgets(BUF, max, com1) != nullptr){
-                        fputs(BUF, com2);
-                }
-
-                pclose(com1);
-                fclose(com2);
-
-                return true;
-        }
 }
 
